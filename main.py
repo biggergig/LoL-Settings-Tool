8dd5112ee1f23b2a855b13512bb2913717d40be4
biggergig <anshuman.d6@gmail.com>
2019-04-23 22:22:28 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+import sys
+import time
+from tkinter import filedialog
+import tkinter
+
+default_path = "C:\Riot Games\\League of Legends"
+
+file_content = ""
+
+def get_path():
+    tkinter.Tk().withdraw()
+    browse_path = filedialog.askdirectory()
+    return (browse_path)
+
+def write_file(content):
+    with open("path.txt", 'w') as a:
+        a.write(content)
+
+
+with open("path.txt",'r') as a:
+    try:
+        file_content = a.read().split()[0]
+    except:
+        file_content = "null"
+
+try:
+    os.chdir(file_content)
+    riot_dir = True
+except:
+    riot_dir = False
+
+if not riot_dir:
+    given_path = get_path()
+    while "League of Legends" not in given_path:
+        given_path = get_path()
