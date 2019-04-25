737dec58ade69d2008bbc0024f13ecf4efee0371
biggergig <anshuman.d6@gmail.com>
2019-04-24 18:34:12 -0700

    update

--- main.py
+++ main.py
@@ -8,19 +8,21 @@ default_path = "C:\Riot Games\\League of Legends"
 
 file_content = ""
 
+basePath = os.getcwd()
+
 def get_path():
     tkinter.Tk().withdraw()
     browse_path = filedialog.askdirectory()
     return (browse_path)
 
 def write_file(content):
-    with open("path.txt", 'w') as a:
+    with open(basePath+"\path.txt", 'w') as a:
         a.write(content)
 
 
 with open("path.txt",'r') as a:
     try:
-        file_content = a.read().split()[0]
+        file_content = a.read().split("\n")[0]
     except:
         file_content = "null"
 
@@ -32,10 +34,12 @@ except:
 
 if not riot_dir:
     given_path = get_path()
-    while "League of Legends" not in given_path:
+    while not given_path.endswith("League of Legends"):
         given_path = get_path()
     os.chdir(given_path)
 
 sub_path = "\Config\PersistedSettings.json"
 print(os.getcwd())
 write_file(os.getcwd())
+
+os.chdir(os.getcwd()+"\Config")
